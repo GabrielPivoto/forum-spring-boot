ac95e1e6a7965cf0f8c21316e07ddd6983e13012
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-09-14 15:38:24 -0300

    Adicionando fumétodo POST

--- src/main/java/br/com/alura/forum/controller/TopicosController.java
+++ src/main/java/br/com/alura/forum/controller/TopicosController.java
@@ -1,22 +1,26 @@
 package br.com.alura.forum.controller;
 
 import br.com.alura.forum.controller.dto.TopicoDto;
-import br.com.alura.forum.modelo.Curso;
+import br.com.alura.forum.controller.form.TopicoForm;
 import br.com.alura.forum.modelo.Topico;
+import br.com.alura.forum.repository.CursoRepository;
 import br.com.alura.forum.repository.TopicoRepository;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
 @RestController //Já assume que todos os métodos vão conter @ResponseBody
+@RequestMapping("/topicos")
 public class TopicosController {
 
     @Autowired
     private TopicoRepository topicoRepository;
 
-    @RequestMapping("/topicos") //Endereço do mapeamento
+    @Autowired
+    private CursoRepository cursoRepository;
+
+   @GetMapping //Endereço do mapeamento
     public List<TopicoDto> lista(String nomeCurso){
         if(nomeCurso == null){
             List<Topico> topicos = topicoRepository.findAll();
@@ -27,6 +31,13 @@ public class TopicosController {
         }
 
         //return Arrays.asList(topico,topico,topico);
+    }
+
+    @PostMapping
+    public void cadatrar(@RequestBody TopicoForm form){
+        Topico topico = form.conveter(cursoRepository);
+        topicoRepository.save(topico);
+
     }
 
 }
