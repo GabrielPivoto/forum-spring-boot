7065df747ad85700e455b9885916bf18249204f6
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-09-20 14:39:39 -0300

    Adicionando configurações de segurança para permitir apenas GET

---
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -0,0 +1,35 @@
+package br.com.alura.forum.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity //habilita o módulo de segurança
+@Configuration //Spring le e carrega as configurações presentes nessa classe
+public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
+
+    //Configurações de autorização
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        //Permite apenas requisições GET
+        http.authorizeRequests()
+                .antMatchers(HttpMethod.GET,"/topicos").permitAll()
+                .antMatchers(HttpMethod.GET,"/topicos/*").permitAll();
+    }
+
+    //Configurações de recursos estáticos (requisições para arquivos)
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+
+    }
+
+    //Configurações de autenticação
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+    }
+}
