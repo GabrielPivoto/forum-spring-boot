fdf1afee39c70a6d6201a966541a71a6fe7d0285
Gabriel Pivoto Ambrósio <gabriel-pivoto@outlook.com>
2022-09-22 10:05:42 -0300

    Adicionando configurações do swagger

--- src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
+++ src/main/java/br/com/alura/forum/config/security/SecurityConfigurations.java
@@ -53,7 +53,8 @@ public class SecurityConfigurations extends WebSecurityConfigurerAdapter {
     //Configurações de recursos estáticos (requisições para arquivos)
     @Override
     public void configure(WebSecurity web) throws Exception {
-
+        web.ignoring()
+                .antMatchers("/**.html", "/v2/api-docs", "/webjars/**", "/configuration/**", "/swagger-resources/**");
     }
 
     //Configurações de autenticação
